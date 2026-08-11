d2e035aa59db6515a684c3d24c50587570557065
prasun301 <prasun301@gmail.com>
2026-08-11 20:55:00 +0600

    Add main JavaScript file for Electrical Engineering site

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+ * ELECTRICAL.PRASUNBARUA.COM
+ * Main JavaScript
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Electrical Engineering website loaded.");
+});
